f7c0cdda51f9a726afcc08bcd2457ab9ee39b560
catuchi <atuchibueze@gmail.com>
2024-07-26 11:39:17 -0400

    equal sides of an array

---
+++ src/EqualSidesOfAnArray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.io.*;
+
+public class EqualSidesOfAnArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] intArr = {20,10,-80,10,10,15,35};
+		System.out.println(findEvenIndex(intArr));
+	}
+	
+	public static int findEvenIndex(int[] arr) {
+		
+		for (int i = 0; i < arr.length; i++) {
+			int[] leftArr = Arrays.copyOfRange(arr, 0, i);
+			int leftSum = Arrays.stream(leftArr).sum();
+			
+			int[] rightArr = Arrays.copyOfRange(arr, i + 1, arr.length);
+			int rightSum = Arrays.stream(rightArr).sum();
+			
+			if (leftSum == rightSum)
+				return i;
+		}
+		return -1;
+	}
+
+}

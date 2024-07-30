213fadbcb1dc8dd8f45e1e0553d461f830d904b0
catuchi <atuchibueze@gmail.com>
2024-07-30 10:53:59 -0400

    break camel casing

---
+++ src/BreakCamelCase.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class BreakCamelCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "camelCasing";
+		System.out.println(camelCase(str));
+	}
+	
+	public static String camelCase(String input) {
+		// loop through string
+		// create result array
+		// at each index check if char at index equal to capitalized char at index
+		// if it is push a space then push the index
+		
+		ArrayList<Character> withSpace = new ArrayList<>();
+		for (char c : input.toCharArray()) {
+			
+			if (c != Character.toUpperCase(c)) {
+				withSpace.add(c);
+			} else {
+				withSpace.add(' ');
+				withSpace.add(c);
+			}
+
+		}
+		
+		StringBuilder sb = new StringBuilder(withSpace.size());
+		
+		for (Character ch : withSpace) {
+			sb.append(ch);
+		}
+		
+//		String result = sb.toString();
+		
+		return sb.toString();
+	}
+
+}
